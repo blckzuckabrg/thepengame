fff93fd0775be865411ca440223e724accb22b76
Aaron Butler <butlera10@gmail.com>
2016-04-27 08:50:56 -0700

    Added Amazon S3 for Paperclip uploads to Heroku

--- app/models/audio_upload.rb
+++ app/models/audio_upload.rb
@@ -3,5 +3,5 @@ class AudioUpload < ActiveRecord::Base
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 	has_attached_file :audio 
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
-	validates_attachment_content_type :audio, :content_type => [ 'audio/MP3', 'audio/Wav', 'audio/Ogg']
+	validates_attachment_content_type :audio, :content_type => [ 'audio/mp3', 'audio/wav', 'audio/ogg']
 end
